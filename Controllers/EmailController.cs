cfb8e6db3b5f6e4a62acd0b515b4254cd175e9c5
Steve Bogucki <sbogucki@mail.usf.edu>
2019-01-28 15:26:08 -0800

    successfully tested sendgrid

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Bogoodski2019.Controllers
+{
+    public class EmailController : Controller
+    {
+
+        public class Message 
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Subject { get; set; }
+            public string Body { get; set; }
+        }
+
+        [Route("/api/sendemail")]
+        [HttpPost]
+        public async Task<IActionResult> SendMail([FromBody] Message message)
+        {
+            var key = System.Environment.GetEnvironmentVariable("SENDGRID_KEY");
+            var apiKey = key;
+            var client = new SendGridClient(apiKey);
+            string email = message.Email;
+            string name = message.Name;
+            string subject = message.Subject;
+            string body = message.Body;
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress($"{email}", $"{name}"),
+                Subject = $"{subject}",
+                PlainTextContent = "test",
+                HtmlContent = $"{body}"
+            };
+            msg.AddTo(new EmailAddress("sbogucki@mail.usf.edu", "Steve Bogucki"));
+            var response = await client.SendEmailAsync(msg);
+
+            return Ok();
+        }
+    }
+}

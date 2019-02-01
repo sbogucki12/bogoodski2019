adf9393521ff6c6e1d1a815335ca1c56281b0eff
Steve Bogucki <sbogucki@mail.usf.edu>
2019-01-31 19:39:18 -0800

    working on running component for main

--- ClientApp/src/components/runLog/RunMain.jsx
+++ ClientApp/src/components/runLog/RunMain.jsx
@@ -1,16 +1,42 @@
 ﻿import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import './RunStyle.css';
-import VisibilitySensor from 'react-visibility-sensor';
 
 const styles = theme => ({
     root: {
         flexGrow: 1,
-        display: "flex",
-        justifyContent: "center"
+        display: "flex", 
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        height: '70vh'
     },
-    runner: {
-        fontSize: '10vh'
+    hiddenCellLeft: {
+        flex: '0 0 15%'
+    },
+    leftColumn: {
+        flex: '0 0 85%',
+        alignSelf: 'flex-end'
+    }, 
+    hiddenCellRight: {
+        paddingTop: '10%',
+        flex: '0 0 30%'
+    },
+    rightTop: {
+        flex: '0 0 10%', 
+        marginLeft: theme.spacing.unit
+    }, 
+    bottomRight: {
+        flex: '0 0 60%',
+        marginLeft: theme.spacing.unit
+    }, 
+    runText: {
+        fontFamily: `'Montserrat', sans-serif`, 
+        
+    },
+    button: {
+        margin: theme.spacing.unit
     }
 });
 
@@ -32,23 +58,29 @@ class RunMain extends React.Component {
         const { classes } = this.props;
         
         const RunnerIcon = String.fromCodePoint(0x1F3C3);
-        let runner;
-
-        const onChange = function (isVisible)  {
-            if (isVisible != 'true') {
-                runner =
-                    console.log("not visible")
-            } else {
-                runner = console.log("visible")
-
-            }
-        };
+        const runner =
+            <div className="runner">
+                {RunnerIcon}
+            </div>;
 
         return (
 
             <div className={classes.root}>
-                <VisibilitySensor onChange={onChange} scrollCheck={true} />
-                {runner}
+                <div className={classes.hiddenCellLeft} />
+                <div className={classes.leftColumn}>
+                    {runner}
+                </div>
+                <div className={classes.hiddenCellRight} />
+                <div className={classes.rightTop}>
+                    <h3 style={{ fontFamily: `'Montserrat', sans-serif`}}>
+                        {`{ run with me }`}
+                    </h3>
+                </div>
+                <div className={classes.bottomRight}>
+                    <Button variant="contained" color="primary" size="small" className={classes.button}>
+                        {`Click to View`}
+                    </Button>
+                </div>
             </div>
         );
     }

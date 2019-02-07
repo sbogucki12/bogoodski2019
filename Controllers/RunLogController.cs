da70182433bbb0957c1652143838358f8361815b
Steve Bogucki <sbogucki@mail.usf.edu>
2019-02-06 19:24:39 -0800

    picture upload post api success

--- Controllers/RunLogController.cs
+++ Controllers/RunLogController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +18,11 @@ namespace Bogoodski2019.Controllers
     [ApiController]
     public class RunLogController : ControllerBase
     {
+        public static IHostingEnvironment _environment;
+        public RunLogController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
 
         [HttpGet]
         [Route("api/runlog/getimage")]
@@ -28,7 +36,27 @@ namespace Bogoodski2019.Controllers
             catch
             {
                 return NotFound();
-            }   
+            }
         }
+
+        [HttpPost]
+        [Route("api/run/postimage")]
+        public string Post(IFormFile file)
+        {
+            try
+            {
+                var domain = _environment.ContentRootPath;
+                using (FileStream filestream = System.IO.File.Create(domain + "/App_Data/" + file.FileName))
+                {
+                    file.CopyTo(filestream);
+                    filestream.Flush();
+                    return string.Format("uploaded to {0}", domain + "\\App_Data"); 
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }      
     }
 }

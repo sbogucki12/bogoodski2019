4fee6cea5e03d741ca9e456cdb2a61e29d0d23ca
Steve B <sbogucki@mail.usf.edu>
2019-02-28 21:10:05 -0800

    laying out blog post template

---
+++ ClientApp/src/components/blog/posts/BlogPostTopBar.jsx
@@ -0,0 +1,40 @@
+﻿import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    background: {
+        minWidth: '100%',
+        minHeight: '100%'        
+    },
+    topBar: {
+        marginTop: '18vh',
+        minWidth: '100%',
+        backgroundColor: '#FFC0CB',
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    topBarLeftBox: {
+        margin: theme.spacing.unit,
+        padding: theme.spacing.unit,
+        marginLeft: '5vw',
+        background: 'rgba(0, 0, 0, 0.5)',
+        color: '#FFFF00'
+    }
+});
+
+function BlogPostTopBar(props) {
+    const { classes } = props;
+
+    return (
+        <div className={classes.background}>
+            <div className={classes.topBar}>
+                <div className={classes.topBarLeftBox}>
+                    {`Thoughts On...`}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default withStyles(styles)(BlogPostTopBar);
---
+++ ClientApp/src/components/blog/posts/MainContentTopBar.jsx
@@ -0,0 +1,34 @@
+﻿import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    mainContentTopBar: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        marginTop: '2%',
+        marginBottom: '2%'
+    },
+    dateBox: {
+        backgroundColor: theme.palette.secondary.main,
+        color: '#FFFFFF',
+        fontStyle: 'italic',
+        borderColor: '#000000',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        padding: theme.spacing.unit
+    }
+});
+
+function MainContentTopBar(props) {
+    const { classes } = props;
+
+    return (
+        <div class={classes.mainContentTopBar}>
+            <div className={classes.dateBox}>
+                {`Feb. 28, 2019`}
+            </div>
+        </div>
+    );
+}
+
+export default withStyles(styles)(MainContentTopBar);

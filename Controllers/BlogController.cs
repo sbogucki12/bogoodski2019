14c55fe3fd82d007757a667d9df52f1f6c580d49
Steve B <sbogucki@mail.usf.edu>
2019-04-17 14:55:32 -0700

    added blog content server side

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogoodski2019.BlogContent;
+using Bogoodski2019.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bogoodski2019.Controllers
+{
+    
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/getblog")]
+        public IActionResult GetBlog(int id)
+        {
+            if(id == 0)
+            {
+                BlogClass blog = WhatHappenedInDecember.blog;
+                return Ok(blog);
+            }
+
+            return NotFound("Blog Not Found");
+        }
+
+    }
+}
---
+++ Models/BlogClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bogoodski2019.Models
+{
+    public class BlogClass
+    {
+        public int Id { get; set; }
+        public string BlogTitle { get; set; }
+        public string PublishDate { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogLink { get; set; }
+
+        public BlogClass(int id, string blogTitle, string publishDate, string blogContent, string blogLink)
+        {
+            Id = id;
+            BlogTitle = blogTitle;
+            PublishDate = publishDate;
+            BlogContent = blogContent;
+            BlogLink = blogLink;
+        }
+    }
+}

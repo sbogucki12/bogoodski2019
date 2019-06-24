b7f7902073589186b09ba97da8fb19406955efaf
Steve B <sbogucki@mail.usf.edu>
2019-06-23 18:54:08 -0700

    more work on the image carousel for bogoodski writes

--- ClientApp/src/components/bogoodskiWrites/WritesMainDesktop.jsx
+++ ClientApp/src/components/bogoodskiWrites/WritesMainDesktop.jsx
@@ -17,84 +17,127 @@ const styles = ({
         justifyContent: 'center',
         flexDirection: 'column',
         width: '70%', 
-        height: 300, 
+        height: '70vh', 
         background: 'rgba(0, 215, 170, .1)',
         borderStyle: 'solid',
         borderColor: '#000000',
         borderWidth: 5,
-        borderRadius: 25
+        borderRadius: 25, 
+        marginTop: '5vh'
     }, 
     imageRow: {
         display: 'flex', 
         justifyContent: 'center', 
         alignItems: 'center', 
-        flexDirection: 'row'
+        flexDirection: 'row', 
+        width: '100%', 
+        marginBottom: '5%', 
+        height: '100%'
+    }, 
+    pageBox: {
+        width: '60%',
+        height: '90%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderStyle: 'solid',
+        borderColor: '#FFFFFF',
+        borderWidth: 5,
+        borderRadius: 5, 
+        margin: '5%'
     }
 });
 
 const WritesMainDesktop = props => {
     const { classes } = props;
 
-    const [state, setSelectedPage] = useState({ selectedPage: 0 }); 
+    const [selectedPage, setSelectedPage] = useState(0); 
+    const [isPageDisplayed, setIsPageDisplayed] = useState(false)
     
-    const pages = ['cover', 'page1', 'page2', 'page3', 'page4', 'page5'];
+    const pages = ['the cover', 'page1', 'page2', 'page3', 'page4', 'page5'];
 
-    const pageToDisplay = pages[state.selectedPage];
-    const nextPageToDisplay = pages[state.selectedPage + 1];
-    const lastPageDisplayed = pages[state.selectedPage - 1];
-
-    let buttonText = 'Read'; 
-    if (state.selectedPage > 0) {
-        buttonText = 'Next Page'
-    }
-
-    let prevPageComponent = 
-        <div>
-            <button onClick={e => getPrevPage(e)}>
-                {`Prev Page`}
-            </button>
-        </div>
-    if (state.selectedPage === 0) {
-        prevPageComponent = null;
-    }
+    const pageToDisplay = pages[selectedPage];
+    const nextPageToDisplay = pages[selectedPage + 1];
+    const lastPageDisplayed = pages[selectedPage - 1];    
 
     const getNextPage = e => {
         e.preventDefault();
-        let currentPage = state.selectedPage;
+        let currentPage = selectedPage;
         let nextPage = currentPage + 1; 
         if (nextPage > pages.length - 1) {
             nextPage = 0;
         }
-        setSelectedPage({ selectedPage: nextPage })
+        setSelectedPage(nextPage)
     }
 
     const getPrevPage = e => {
         e.preventDefault();
-        let currentPage = state.selectedPage;
+        let currentPage = selectedPage;
         let prevPage = currentPage - 1;        
-        setSelectedPage({ selectedPage: prevPage })
+        setSelectedPage(prevPage)
+    }
+
+    let nextPageComponent =
+        <span style={{ opacity: .3, width: '20%', textAlign: 'center' }} onClick={e => getNextPage(e)}>
+            {nextPageToDisplay}
+        </span>;
+    if (selectedPage === 0 && isPageDisplayed === false) {
+        nextPageComponent = <span style={{ width: '20%' }}>{``}</span>;
+    }
+    if (selectedPage === pages.length -1) {
+        nextPageComponent = <span style={{ width: '20%' }}>{``}</span>
+    }
+
+    const toDisplayPage = e => {
+        e.preventDefault(); 
+        setIsPageDisplayed(true);
+    }
+
+    let displayPageComponent =
+        <div>
+            {`Loading...`}
+        </div>;
+    if (isPageDisplayed === false) {
+        displayPageComponent =
+            <p onClick={e => toDisplayPage(e)}>
+                {pageToDisplay}
+            </p>
+    }
+
+    if (isPageDisplayed === true) {
+        displayPageComponent =
+            <div className={classes.pageBox}>
+                {`This is ${pages[selectedPage]}`}
+            </div>
+    }
+
+    let hideDisplayPage = null; 
+
+    if (isPageDisplayed) {
+        hideDisplayPage =
+            <button onClick={() => setIsPageDisplayed(false)} style={{ marginBottom: '2%' }}>
+                {`Close Book`}
+            </button>
+    }
+
+    let lastPageDisplayedComponent =
+        <span style={{ opacity: .3, width: '20%', textAlign: 'center' }} onClick={e => getPrevPage(e)}>
+            {lastPageDisplayed}
+        </span>;
+
+    if (selectedPage === 0) {
+        lastPageDisplayedComponent = <span style={{ width: '20%' }}>{``}</span>;
     }
 
     const content = (
         <div className={classes.root}>
             <div className={classes.mainBox}>
                 <div className={classes.imageRow}>
-                    <p style={{ marginRight: '10%', opacity: .3 }}>
-                        {lastPageDisplayed}
-                    </p>
-                    <p>
-                        {pageToDisplay}
-                    </p>
-                    <p style={{ marginLeft: '10%', opacity: .3 }}>
-                        {nextPageToDisplay}
-                    </p>
+                    {lastPageDisplayedComponent}
+                    {displayPageComponent}
+                    {nextPageComponent}
                 </div>
-                <div className={classes.imageRow}>
-                    {prevPageComponent}
-                    <button onClick={e => getNextPage(e)}>
-                        {buttonText}
-                    </button>
-                </div>
+                    {hideDisplayPage}
             </div>
         </div>
     ); 
